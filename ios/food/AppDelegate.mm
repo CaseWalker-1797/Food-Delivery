9fac0c47260aeb0bf6e96ef54ca3fb62568b3cee
Veer J Kamdar <veerpersonal1797@gmail.com>
2024-01-29 15:58:23 +0530

    Revert "Add Authentication"
    This reverts commit 230dd836a40f9d1915d17678ccf306bd93b933c6.

--- ios/food/AppDelegate.mm
+++ ios/food/AppDelegate.mm
@@ -1,9 +1,6 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <AuthenticationServices/AuthenticationServices.h>
-#import <SafariServices/SafariServices.h>
-#import <FBSDKCoreKit/FBSDKCoreKit-Swift.h>
 
 @implementation AppDelegate
 
@@ -13,9 +10,6 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  //Facebook Login Button
-     [[FBSDKApplicationDelegate sharedInstance] application:application
-                       didFinishLaunchingWithOptions:launchOptions];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
