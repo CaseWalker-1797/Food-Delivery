585f7c6bf826b3b76696319bd33e919458f8e112
Case Walker <veerkamdar123@gmail.com>
2024-02-02 10:02:21 +0530

    Login Logout Function Working

--- src/navigations/navigation.js
+++ src/navigations/navigation.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/appStack/HomeScreen";
 import RestroScreen from "../screens/appStack/RestroScreen";
-
 import CartScreen from "../screens/appStack/CartScreen";
 import PreparingOrderScreen from "../screens/appStack/PreparingOrderScreen";
 import DeliveryScreen from "../screens/appStack/DeliveryScreen";
@@ -19,7 +18,7 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="User"
+        initialRouteName="Home"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Login" component={LoginScreen} />
@@ -42,7 +41,11 @@ export default function Navigation() {
           options={{ presentation: "fullScreenModal", headerShown: false }}
           component={DeliveryScreen}
         />
-        <Stack.Screen name="User" component={UserProfileScreen} />
+        <Stack.Screen
+          name="User"
+          options={{ headerShown: false }}
+          component={UserProfileScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/appStack/UserProfileScreen.js
+++ src/screens/appStack/UserProfileScreen.js
@@ -3,13 +3,28 @@ import React from "react";
 import * as Icon from "react-native-feather";
 import { themeColors } from "../../styles/theme";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { urlFor } from "../../../sanity";
+import { useNavigation } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
 
 const UserProfileScreen = () => {
+  const navigation = useNavigation();
+  const logout = () => {
+    auth()
+      .signOut()
+      .then(() => console.log("User signed out!"));
+    navigation.navigate("Login");
+  };
   return (
     <SafeAreaView className="flex-1 bg-gray-100 justify-center">
+      {/* Back Button */}
+      <TouchableOpacity
+        onPress={() => navigation.goBack()}
+        className="absolute border  border-gray-100 top-12 left-6 bg-gray-50 p-2 rounded-full shadow"
+      >
+        <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
+      </TouchableOpacity>
+      {/* Profile Picture */}
       <Image
-        style={{ backgroundColor: "rgba(255,255,255,0.4)" }}
         className=" w-36 h-36 self-center rounded-full m-2 p-2 "
         source={require("../../assets/images/deliveryGuy.jpeg")}
       />
@@ -59,32 +74,29 @@ const UserProfileScreen = () => {
             strokeWidth="2.5"
             stroke="white"
           />
-          <Text className="text-base text-white font-bold text-center">FAQs</Text>
+          <Text className="text-base text-white font-bold text-center">
+            FAQs
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* Your Account Button */}
+      <View className=" flex-row justify-center items-center m-1 p-2 ">
+        <TouchableOpacity
+          className=" flex-row flex-1 justify-between items-center bg-white border-b-2 border-gray-200 rounded-lg p-4 m-1 "
+          onPress={() => navigation.navigate("User")}
+        >
+          <Icon.User height={20} width={20} strokeWidth="2.5" stroke="black" />
+          <Text className="text-base font-semibold">Your Account</Text>
+          <Icon.ChevronRight
+            height={20}
+            width={20}
+            strokeWidth="2.5"
+            stroke="black"
+          />
         </TouchableOpacity>
       </View>
       {/* Other Options */}
       <View className=" bg-white space-y-2 m-4 p-2 rounded-lg">
-        {/* Your Account Button */}
-        <View className=" flex-row justify-center items-center m-1 p-2 ">
-          <TouchableOpacity
-            className=" flex-row flex-1 justify-between items-center bg-white border-b-2 border-gray-200 rounded-lg p-4 "
-            onPress={() => navigation.navigate("User")}
-          >
-            <Icon.User
-              height={20}
-              width={20}
-              strokeWidth="2.5"
-              stroke="black"
-            />
-            <Text className="text-base font-semibold">Your Account</Text>
-            <Icon.ChevronRight
-              height={20}
-              width={20}
-              strokeWidth="2.5"
-              stroke="black"
-            />
-          </TouchableOpacity>
-        </View>
         {/* Address Book */}
         <View className=" flex-row justify-center items-center m-1 p-2 ">
           <TouchableOpacity
@@ -118,7 +130,9 @@ const UserProfileScreen = () => {
               strokeWidth="2.5"
               stroke="black"
             />
-            <Text className="text-base font-semibold">Manage Payment Methods</Text>
+            <Text className="text-base font-semibold">
+              Manage Payment Methods
+            </Text>
             <Icon.ChevronRight
               height={20}
               width={20}
@@ -149,22 +163,22 @@ const UserProfileScreen = () => {
           </TouchableOpacity>
         </View>
       </View>
-       {/* Logout Button */}
-        <View className=" flex-row justify-center items-center m-1 p-2 ">
-          <TouchableOpacity
-            style={styles.container}
-            className="flex-row flex-1 space-x-2 bg-white justify-center items-center rounded-full shadow-lg p-3"
-            onPress={() => navigation.navigate("User")}
-          >
-            <Icon.LogOut
-              height={20}
-              width={20}
-              strokeWidth="2.5"
-              stroke="black"
-            />
-            <Text className="text-base font-semibold">Logout</Text>
-          </TouchableOpacity>
-        </View>
+      {/* Logout Button */}
+      <View className=" flex-row justify-center items-center m-1 p-2 ">
+        <TouchableOpacity
+          style={styles.container}
+          className="flex-row flex-1 space-x-2 bg-white justify-center items-center rounded-full shadow-lg p-3"
+          onPress={logout}
+        >
+          <Icon.LogOut
+            height={20}
+            width={20}
+            strokeWidth="2.5"
+            stroke="black"
+          />
+          <Text className="text-base font-semibold">Logout</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
